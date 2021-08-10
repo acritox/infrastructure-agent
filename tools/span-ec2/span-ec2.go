6eb5ae14f159ce6bb987e0427b3e1fb034db5f0f
Grigorii Merkushev <brushknight@gmail.com>
2021-08-10 16:15:27 +0200

    wip: add basic UI

--- tools/span-ec2/span-ec2.go
+++ tools/span-ec2/span-ec2.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
+	"os"
+	"strconv"
 )
 
 const instancesFile = "../../test/automated/ansible/group_vars/localhost/main.yml"
 
+type Config struct {
+	Instances []instanceDef `yaml:"instances"`
+}
+
 type instanceDef struct {
-	ami               string `yaml:"ami"`
-	instanceType      string `yaml:"type"`
-	name              string `yaml:"name"`
-	username          string `yaml:"username"`
-	pythonInterpreter string `yaml:"python_interpreter"`
-	launchTemplate    string `yaml:"launch_template"`
+	Ami               string `yaml:"ami"`
+	InstanceType      string `yaml:"type"`
+	Name              string `yaml:"name"`
+	Username          string `yaml:"username"`
+	PythonInterpreter string `yaml:"python_interpreter"`
+	LaunchTemplate    string `yaml:"launch_template"`
 }
 
 func main() {
@@ -23,12 +30,50 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	var instances []instanceDef
-	err = yaml.Unmarshal(yamlFile, &instances)
+	config := Config{}
+	err = yaml.Unmarshal(yamlFile, &config)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
+	optionFormat := "[%2d] %25s"
 
+	fmt.Printf("Choose AMI to create\n\n")
+	for i, instance := range config.Instances{
+		fmt.Println(fmt.Sprintf(optionFormat,i, instance.Name))
+	}
+
+	fmt.Print("Select one of numbers: ")
+
+	// get user input
+	var userInput string
+
+	fmt.Scanln(&userInput)
+
+	chosenAmiNumber, err := strconv.Atoi(userInput)
+
+	if err != nil{
+		panic(err)
+	}
+
+	// validate input
+
+	// confirm
+	fmt.Printf("Chosen AMI %d - %s\nIs this right [(y)es/(n)o]: ", chosenAmiNumber, config.Instances[chosenAmiNumber].Name)
+
+	fmt.Scanln(&userInput)
+
+	if userInput != "yes" && userInput != "y"{
+		os.Exit(0)
+	}
+
+	// prepare ansible config (tmp list of hosts to create)
+	fmt.Printf("Preparing config for %s\n", config.Instances[chosenAmiNumber].Name)
+
+
+	// execute ansible
+	fmt.Printf("Executing Ansible for %s\n", config.Instances[chosenAmiNumber].Name)
 
 }
+
+
